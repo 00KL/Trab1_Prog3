f1685c975d9c41e909bacf5129bddd7392e381c7
00KL <lucasmoraesplay@gmail.com>
2018-09-26 12:01:31 -0300

    Add files via upload

---
+++ Trab1_Prog3/src/eleicao/candidato.java
@@ -0,0 +1,22 @@
+package eleicao;
+
+public class candidato {
+//Seq.(i);Núm.;Candidato;Partido/Coligação;Votação;% Válidos
+//	Número de vagas;
+//	• Candidatos eleitos (sempre indicado partido, número de votos e coligação, se houver)
+//	• Candidatos mais votados dentro do número de vagas;
+//	• Candidatos não eleitos e que seriam eleitos se a votação fosse majoritária;
+//	• Candidatos eleitos no sistema proporcional vigente, e que não seriam eleitos se a votação fosse
+//	majoritária, isto é, pelo número de votos apenas que um candidato recebe diretamente;
+//	• Votos totalizados por coligação ou partido (quando um partido não estiver em coligação), número de
+//	candidatos eleitos;
+//	• Votos totalizados por partido, número de candidatos eleitos;
+//	• Total de votos nominais.
+	
+	private char situacao;
+	private int num;
+	private String nome;
+	private String partido_colicagacao;
+	private int votos;
+	private int validados;
+}
